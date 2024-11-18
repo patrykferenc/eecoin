96d9592d951ee3e0eddb5e665b7cbd49bac1a340
michalskim <72550341+legeof008@users.noreply.github.com>
2024-11-18 13:07:39 +0100

    Wip/14 simple mining blockchain persistence (#17)
    * tidy (chore) #12
    
    * tests expanded; code cleanup; (chore) #12
    
    * typo; (chore) #12
    
    * feat: add blockchain persistence to file
    
    ---------
    
    Co-authored-by: patrykferenc <patrykferenc@protonmail.com>

---
+++ internal/blockchain/domain/blockchain/persistence_test.go
@@ -0,0 +1,34 @@
+package blockchain
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestPersistBlockChain(t *testing.T) {
+	assertThat := assert.New(t)
+
+	// given
+	dir := t.TempDir() + "/chain"
+	genesis := GenerateGenesisBlock()
+	chain, _ := ImportBlockchain([]Block{genesis})
+
+	// and given
+	timestamp := time.Date(2023, 2, 3, 12, 0, 0, 0, time.UTC).UnixMilli()
+	transactions := make([]TransactionID, 0)
+	newBlock, _ := chain.NewBlock(timestamp, transactions)
+	_ = chain.AddBlock(newBlock)
+
+	// when - then
+	saveErr := persist(*chain, dir)
+	assertThat.Nil(saveErr)
+
+	//when - then
+	loaded, err := load(dir)
+
+	assertThat.Nil(err)
+	assertThat.NotNil(loaded)
+	assertThat.Equal(loaded.GetFirst(), chain.GetFirst())
+	assertThat.Equal(loaded.GetLast(), chain.GetLast())
+}
