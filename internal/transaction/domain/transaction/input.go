be822893a0361e28ca8679ad038ebc07cdd43fac
patrykferenc <patrykferenc@protonmail.com>
2024-12-09 16:13:07 +0100

    fix lint #30

--- internal/transaction/domain/transaction/input.go
+++ internal/transaction/domain/transaction/input.go
@@ -16,7 +16,7 @@ type Input struct {
 func (i *Input) sign(signer crypto.Signer, idToSign ID, referencedOutput UnspentOutput) error {
 	ourAddress, err := x509.MarshalPKIXPublicKey(signer.Public())
 	if err != nil {
-		fmt.Errorf("error marshalling public key: %w", err)
+		return fmt.Errorf("error marshalling public key: %w", err)
 	}
 	if string(ourAddress) != referencedOutput.address {
 		return fmt.Errorf("output address does not match the signer address")
